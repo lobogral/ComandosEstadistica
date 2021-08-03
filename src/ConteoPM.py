d000b5769f43e091c20eea216046a1a3f9bc305e
Camilo Sabogal <lobogral@gmail.com>
2021-08-03 11:40:21 -0500

    Dejo claro que prod debe ser un método privado

--- src/ConteoPM.py
+++ src/ConteoPM.py
@@ -1,20 +1,20 @@
+def __prod(lista):
+    if (lista == []):
+        return 1
+    else:
+        return lista[0]*__prod(lista[1:])
+
 def factorial(n):
     if (n > 1):
         return n * factorial(n-1)
     else:
         return 1
 
-def prod(lista):
-    if (lista == []):
-        return 1
-    else:
-        return lista[0]*prod(lista[1:])
-
 def P(n,r):
     return factorial(n) // factorial (n-r)
 
 def PClase(n, *args):
-    return factorial(n) // prod([factorial(i) for i in args])
+    return factorial(n) // __prod([factorial(i) for i in args])
 
 def C(n,r):
     return factorial(n) // (factorial(r) * factorial(n-r))
