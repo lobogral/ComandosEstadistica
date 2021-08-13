0c98dac97734092829b3262d2020678a298b5fd0
Camilo Sabogal <lobogral@gmail.com>
2021-08-13 10:33:13 -0500

    Agrego funciones a distribucion continua

--- src/distribuciones/conjuntaContinua.py
+++ src/distribuciones/conjuntaContinua.py
@@ -1,21 +1,31 @@
-from sympy import integrate, Piecewise, oo, simplify, piecewise_fold
-from sympy.abc import t, x, y
+from sympy import integrate, Piecewise, oo
+from sympy import simplify, piecewise_fold, Symbol
+from sympy.abc import x, y, z
 
-def Prob(función, intervalo):
+def __AgregarIntervalo(función, intervalo):
     trozoImport = función.args[0]
     lista = [] 
     lista += [(trozoImport[0], trozoImport[1] & intervalo)]
     lista += [(0, True)]
-    nuevaFunc = Piecewise(*lista)
-    return integrate(nuevaFunc, (x, -oo, oo), (y, -oo, oo))
+    return Piecewise(*lista)
+
+def ProbTotal(función):
+    variables = función.atoms(Symbol)
+    lista = [(variable, -oo, oo) for variable in variables]
+    return integrate(función, *lista)
+
+def Prob(función, intervalo):
+    nuevaFunc = __AgregarIntervalo(simplify(función), intervalo)
+    return ProbTotal(nuevaFunc)
 
 def ProbMarginal(función, variable):
     variable = y if variable == x else x
     integral = integrate(función, (variable,-oo,oo))
     return simplify(piecewise_fold(integral.rewrite(Piecewise)))
 
-def ProbCondicional(función, valDep1, valDep2, varIndep, valIndep):
+def ProbCondicional(función, intervaloDep, varIndep, valIndep):
     varDep = y if varIndep == x else x
     funcCond = función/ProbMarginal(función, varIndep)
-    funcCondEval = funcCond.subs(varIndep, valIndep)
-    return integrate(funcCondEval, (varDep, valDep1, valDep2))
+    funcCondEval = simplify(funcCond.subs(varIndep, valIndep))
+    funcCondEvalInt = __AgregarIntervalo(funcCondEval, intervaloDep)
+    return integrate(funcCondEvalInt, (varDep, -oo, oo))
