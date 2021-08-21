caf385959ff4cf36ef369c782fa6d8be21e1a2d4
Camilo Sabogal <lobogral@gmail.com>
2021-08-20 20:49:19 -0500

    Retiro variable en metodo

--- src/distribuciones/continua.py
+++ src/distribuciones/continua.py
@@ -1,11 +1,13 @@
-from sympy import Piecewise, oo, simplify, integrate
+from sympy import Piecewise, oo, simplify, integrate, Symbol
 from sympy.stats import P, cdf
 
 Prob = P
 
-def ProbTotal(fdp, variable):
+def ProbTotal(fdp):
+    variable = fdp.atoms(Symbol)
     return integrate(fdp, (variable,-oo,oo))
 
-def ProbAcum(continuaVA, variable):
-    integral = cdf(continuaVA)(variable)
+def ProbAcum(continuaVA):
+    variable = continuaVA.atoms(Symbol)
+    integral = cdf(continuaVA)(*variable)
     return simplify(integral.rewrite(Piecewise))
