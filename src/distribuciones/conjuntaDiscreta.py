e6ac762d541b354951c73b695be862b696dd727f
Camilo Sabogal <lobogral@gmail.com>
2021-08-22 12:33:51 -0500

    Desacoplo dist para mejorar sintaxis en discConj

--- src/distribuciones/conjuntaDiscreta.py
+++ src/distribuciones/conjuntaDiscreta.py
@@ -1,34 +1,43 @@
-from sympy import Piecewise, Symbol
+from sympy import Piecewise, Symbol, solve
+from itertools import product
 
-x = Symbol("x", real=True)
-y = Symbol("y", real=True)
+dist = None
+vars = None
 
-def Func2Dist(función, valoresX, valoresY):
+def establecerDist(distNueva, varsNuevas):
+    global dist
+    global vars
+    dist = distNueva
+    vars = varsNuevas
+
+def Func2Dist(función, vars, *vals):
     return {
-        (valX, valY):función.subs({x:valX, y:valY}) 
-        for valX in valoresX for valY in valoresY
+        k:función.subs(dict(zip(vars,k))) 
+        for k in list(product(*vals))
     }
 
-def ProbTotal(distribución):
-    dicc = distribución
+def ProbTotal(distPrueba):
+    dicc = distPrueba
     return sum([v for k,v in dicc.items()])
 
-def Prob(distribución, área):
-    dicc = distribución
+def Prob(área):
+    dicc = dist
     return sum(
-        [v for (valx,valy),v in dicc.items() 
-        if área.subs({x:valx, y:valy})]
+        [v for k,v in dicc.items() 
+        if área.subs(dict(zip(vars,k)))]
     )
 
-def ProbMarginal(distribución, variable):
-    dicc = distribución
-    num = 0 if variable == x else 1
+def ProbMarginal(varMar):
+    dicc = dist
+    num = vars.index(varMar)
     listaVar = list(set([k[num] for k,v in dicc.items()]))
     suma = lambda val : sum([v for k,v in dicc.items() if k[num] == val])
     return {var:suma(var) for var in listaVar}
 
 
-def ProbCondicional(distribución, varDep, valX, valY):
-    valDep = valX if varDep == x else valY
-    marginal = ProbMarginal(distribución, varDep)
-    return distribución[(valX, valY)]/marginal[valDep]
+def ProbCondicional(eqDep, eqIndep):
+    varIndep, = eqIndep.atoms(Symbol)
+    valIndep, = solve(eqIndep)
+    vals = tuple([solve([eqDep, eqIndep])[var] for var in vars])
+    marginal = ProbMarginal(varIndep)
+    return dist[vals]/marginal[valIndep]
