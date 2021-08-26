f582cf5f41f2f207b85a40ab0b6bbe3918a5fea4
Camilo Sabogal <lobogral@gmail.com>
2021-08-26 17:52:11 -0500

    Cambio dicc por Piecewise en dist

--- src/distribuciones/discreta.py
+++ src/distribuciones/discreta.py
@@ -1,19 +1,31 @@
-from sympy import Piecewise
-from sympy.abc import x
+from sympy import Piecewise, Symbol, summation, Eq
+from sympy.abc import j, x
 
-def Func2Dist(función, variable, valores):
-    return {valor:función.subs(variable, valor) for valor in valores}
+def Func2Troz(dp, dom):
+    var, = dp.atoms(Symbol)
+    lista = [(dp.subs(var,val), Eq(var,val)) for val in dom[var]]
+    return Piecewise(*lista)
 
-def ProbTotal(distribución):
-    dicc = distribución
-    return sum([v for k,v in dicc.items()])
+def ProbTotal(dpPru, domPru):
+    var, = dpPru.atoms(Symbol)
+    if isinstance(domPru[var], tuple):
+        intervalo = (var, *domPru[var])
+        return summation(dpPru, intervalo)
+    else:
+        vals = domPru[var]
+        return sum([dpPru.subs(var, val) for val in vals])
 
-def ProbAcum(distribución):
-    dicc = distribución
+def ProbAcum(dp, dom):
+    var, = dp.atoms(Symbol)
+    if isinstance(dom[var], tuple):
+        vals = range(*[sum(x) for x in zip(dom[var], (0,1))])
+    else:
+        vals = dom[var]
+
     lista = []
-    for i in range(len(dicc)):
-        subSuma = sum([v for k,v in dicc.items()][:i])
-        llave = [k for k,v in dicc.items()][i]
-        lista += [(subSuma, (x<llave))]
+    for i in range(len(dp.args)):
+        subSuma = sum([dp.subs(var,val) for val in vals[:i]])
+        llave = vals[i]
+        lista += [(subSuma, (var<llave))]
     lista += [(1, True)]
     return Piecewise(*lista)
