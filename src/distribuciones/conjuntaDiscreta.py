4a2af503ca2a70124a29d1b12f47f5c11faf3725
Camilo Sabogal <lobogral@gmail.com>
2021-08-29 20:42:50 -0500

    Retiro los dominios de conjunta discreta

--- src/distribuciones/conjuntaDiscreta.py
+++ src/distribuciones/conjuntaDiscreta.py
@@ -1,60 +1,66 @@
-from sympy import Piecewise, Symbol, solve, And
-from sympy import summation, Eq, piecewise_fold, simplify
+from sympy import Piecewise, Symbol, solve, And, Contains, FiniteSet
+from sympy import summation, Eq, piecewise_fold, simplify, oo
 from itertools import product
 
 dp = None
-dom = None
 
-def establecerDpDom(dpNuevo, domNuevo):
+def establecerDp(dpNuevo):
     global dp
-    global dom
     dp = dpNuevo
-    dom = domNuevo
 
 def __AgregarIntervalo(función, area):
     funcionTrozos = Piecewise((función, area),(0, True))
     return piecewise_fold(funcionTrozos)
 
-def ProbTotal(dpPru, domPru):
+def __EstablecerDominio(dp):
+    dom = {var:[] for var in dp.atoms(Symbol)}
+    for eq in dp.atoms(Eq):
+        var, = eq.atoms(Symbol)
+        val = solve(eq, var)
+        dom[var] += val
+    for contain in dp.atoms(Contains):
+        var, = contain.atoms(Symbol)
+        val = list(*contain.atoms(FiniteSet))
+        dom[var] = val
+    return dom
+
+def ProbTotal(dpPru, varsPru=None):
+    if varsPru==None: varsPru=dpPru.atoms(Symbol)
+    domPru = __EstablecerDominio(dpPru)
     prob = dpPru
-    for var in domPru.keys():
-        if isinstance(domPru[var], tuple):
-            intervalo = (var, *domPru[var])
-            prob = summation(prob, intervalo)
-        else:
+    for var in varsPru:
+        if domPru[var]:
             vals = domPru[var]
             prob = sum([prob.subs(var, val) for val in vals])
+        else:
+            prob = summation(prob, (var,0,oo))
     return prob
 
 def Prob(area):
     nuevaFunc = __AgregarIntervalo(dp, area)
-    return ProbTotal(nuevaFunc, dom)
+    return ProbTotal(nuevaFunc)
 
 def ProbMarginal(*varsMar):
     varsDp = dp.atoms(Symbol)
-    domMar = {var:dom[var] for var in varsDp - {*varsMar}}
-    return simplify(ProbTotal(dp, domMar))
+    varsMar = varsDp - {*varsMar}
+    return simplify(ProbTotal(dp, varsMar))
 
 def ProbCondicional(eqsDep, eqsIndep):
-    valsDep = solve(eqsDep.atoms(Eq))
     valsIndep = solve(eqsIndep.atoms(Eq))
     varsIndep = eqsIndep.atoms(Symbol)
     funcCond = dp/ProbMarginal(*varsIndep)
     funcCondEval = simplify(funcCond.subs(valsIndep))
-    funcCondEvalEq = __AgregarIntervalo(funcCondEval, eqsDep)
-    return simplify(funcCondEvalEq.subs(valsDep))
+    funcCondEvalEqs = __AgregarIntervalo(funcCondEval, eqsDep)
+    return ProbTotal(funcCondEvalEqs)
 
-def dp2Dist():
-    vars = dom.keys()
-    vals = [dom[var] for var in vars]
+def dp2Dist(*vars):
+    dom = __EstablecerDominio(dp)
+    vals = [dom[var] for var in [*vars]]
     prodCart = list(product(*vals))
     return {k:dp.subs(dict(zip(vars,k))) for k in prodCart}
 
 def dist2Dp(dist, vars):
     def genEq(tupl):
         return And(*[Eq(k, v) for k, v in zip(tupl, vars)])
-    def genDomVar(dist, var):
-        return list({val[vars.index(var)] for val in dist.keys()})
     listaTrozDp = [(v, genEq(k)) for k, v in dist.items()]
-    listaDoms = {var: genDomVar(dist, var) for var in vars}
-    return Piecewise(*listaTrozDp), listaDoms
+    return Piecewise(*listaTrozDp)
