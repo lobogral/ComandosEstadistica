0af8bfac8e855e4f4110cc26bd7d69c7786255f7
Camilo Sabogal <lobogral@gmail.com>
2021-09-06 13:45:36 -0500

    Agrego var, cov, desv

--- src/esperanza/contE.py
+++ src/esperanza/contE.py
@@ -1,6 +1,25 @@
 from distribuciones import conjuntaContinua as conjCont
+from functools import reduce
+from sympy import Symbol
 
 establecerFdp = conjCont.establecerFdp
 
 def E(func):
     return conjCont.ProbTotal(func*conjCont.fdp)
+
+def Var(func):
+    fdp = conjCont.fdp
+    return conjCont.ProbTotal(((func - E(func))**2)*fdp)
+
+def VarAlter(func):
+    var, = func.atoms(Symbol)
+    return E(var**2) - E(func)**2
+
+def desv(func):
+    return sqrt(Var(func))
+
+def Cov():
+    fdp = conjCont.fdp
+    mulSymbol = reduce(lambda a, b: a*b, fdp.atoms(Symbol))
+    mulEsp = reduce(lambda a, b: E(a)*E(b), fdp.atoms(Symbol))
+    return E(mulSymbol)-mulEsp
--- src/esperanza/distE.py
+++ src/esperanza/distE.py
@@ -1,11 +1,31 @@
 from distribuciones import conjuntaDiscreta as conjDisc
 from distribuciones import discreta as disc
+from functools import reduce
 from sympy import Symbol
+from math import sqrt
 
 establecerDp = conjDisc.establecerDp
 
 def E(func):
-    return conjDisc.ProbTotal(func*conjDisc.dp)
+    dp = conjDisc.dp
+    return conjDisc.ProbTotal(func*dp)
+
+def Var(func):
+    dp = conjDisc.dp
+    return conjDisc.ProbTotal(((func - E(func))**2)*dp)
+
+def VarAlter(func):
+    var, = func.atoms(Symbol)
+    return E(var**2) - E(func)**2
+
+def desv(func):
+    return sqrt(Var(func))
+
+def Cov():
+    dp = conjDisc.dp
+    mulSymbol = reduce(lambda a, b: a*b, dp.atoms(Symbol))
+    mulEsp = reduce(lambda a, b: E(a)*E(b), dp.atoms(Symbol))
+    return E(mulSymbol)-mulEsp
 
 def dist2Dp(dist, vars):
     if(isinstance(vars, Symbol)):
