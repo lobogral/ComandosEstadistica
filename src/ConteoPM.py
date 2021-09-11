abf93d9c655dad40bd4bf617766ad9b3ff0dccdc
Camilo Sabogal <lobogral@gmail.com>
2021-09-10 20:48:30 -0500

    Mejoro nomenclatura

--- src/ConteoPM.py
+++ src/ConteoPM.py
@@ -3,11 +3,29 @@ from math import prod
 def factorial(n):
     return n * factorial(n-1) if n > 1 else 1
 
-def P(n,r):
+def nP(n,r):
+    """
+    Calcula el numero de permutaciones
+    n -> Numero de objetos totales
+    r -> Numero de objetos tomados
+    """
     return factorial(n) // factorial (n-r)
 
-def PClase(n, *args):
-    return factorial(n) // prod([factorial(i) for i in args])
+def nT(n, listArgs):
+    """
+    Calcula el numero de permutaciones de clase
+    n -> Numero de objetos totales
+    listArgs -> 
+      Lista de numeros (n1,...,nk) tales 
+      que n1 + ... + nk = n
+    """
+    mult = prod([factorial(i) for i in listArgs])
+    return factorial(n) // mult
 
-def C(n,r):
+def nC(n,r):
+    """
+    Calcula el numero de combinaciones
+    n -> Numero de objetos totales
+    r -> Numero de objetos tomados
+    """
     return factorial(n) // (factorial(r) * factorial(n-r))
