7c4d17bcaa4d17c50c629686f84fd9458f301e29
Camilo Sabogal <lobogral@gmail.com>
2021-09-10 21:02:24 -0500

    Retiro documentacion

--- src/ConteoPM.py
+++ src/ConteoPM.py
@@ -4,28 +4,11 @@ def factorial(n):
     return n * factorial(n-1) if n > 1 else 1
 
 def nP(n,r):
-    """
-    Calcula el numero de permutaciones
-    n -> Numero de objetos totales
-    r -> Numero de objetos tomados
-    """
     return factorial(n) // factorial (n-r)
 
 def nT(n, listArgs):
-    """
-    Calcula el numero de permutaciones de clase
-    n -> Numero de objetos totales
-    listArgs -> 
-      Lista de numeros (n1,...,nk) tales 
-      que n1 + ... + nk = n
-    """
     mult = prod([factorial(i) for i in listArgs])
     return factorial(n) // mult
 
 def nC(n,r):
-    """
-    Calcula el numero de combinaciones
-    n -> Numero de objetos totales
-    r -> Numero de objetos tomados
-    """
     return factorial(n) // (factorial(r) * factorial(n-r))
