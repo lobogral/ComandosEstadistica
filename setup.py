4b49cfcce207d236060d4f030081648d4c8ee154
Camilo Sabogal <lobogral@gmail.com>
2021-09-30 16:57:54 -0500

    Agrego la funcionalidad editable con pip -e

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="estadistica",
     version="0.1",
-    package_dir={'estadistica': 'src/estadistica'},
-    packages=find_namespace_packages('src'),
+    packages=find_namespace_packages(),
     install_requires = [
         'redondeo @ https://github.com/lobogral/redondeo/releases/latest/download/redondeo.tar.gz',
         'matplotlib',
