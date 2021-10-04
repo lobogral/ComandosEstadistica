c633308c2917231d9695bfa818b0753ce557facf
Camilo Sabogal <lobogral@gmail.com>
2021-10-04 12:28:34 -0500

    Cambio de version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name="estadistica",
-    version="0.1",
+    version="0.2",
     packages=find_namespace_packages(),
     install_requires = [
         'redondeo @ https://github.com/lobogral/redondeo/releases/latest/download/redondeo.tar.gz',
