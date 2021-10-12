1e98971fa7dbff2881927f4da589971d7b98ad76
Camilo Sabogal <lobogral@gmail.com>
2021-10-12 14:26:11 -0500

    hago prueba con puntos con diferentes linters

--- estadistica/graficas/puntos.py
+++ estadistica/graficas/puntos.py
@@ -3,27 +3,35 @@
 from matplotlib import pyplot as plt
 
 
-def dibujar(diccionarios, titulo_ventana, unidad_medida):
-    """
-    Realiza lo descrito en el docstring del modulo.
+def dibujar(diccionarios: list[dict],
+            titulo_ventana: str,
+            unidad_medida: str) -> None:
+    """Realiza lo descrito en el docstring del modulo.
 
-    Parametros
+    Parameters
     ----------
-    diccionarios: Diccionarios que contiene
-    - nombre: Nombre de la muestra
-    - color: Color de los puntos
-    - muestra: Arreglo de datos
-    titulo_ventana: Titulo de la ventana
-    unidad_medida: Unidad de la medida
+    diccionarios
+        Lista de diccionarios con la siguiente estructura
+        {
+            'nombre': nombre,
+            'color': color,
+            'muestra': muestra
+        }
+    titulo_ventana
+        Titulo de la ventana
+    unidad_medida
+        Unidad de la medida
 
     """
-    limite_superior = __establecer_limite_superior(diccionarios)
-    diccionarios = __establecer_valores(diccionarios)
+    muestras = [val for dic in diccionarios for val in dic['muestra']]
+    limite_superior = __establecer_limite_superior(muestras)
+    listas_y = __establecer_valores_y(muestras, len(diccionarios))
+
     plt.figure(titulo_ventana, figsize=(9, 0.9+0.15*limite_superior))
-    for diccionario in diccionarios:
+    for diccionario, lista_y in zip(diccionarios, listas_y):
         color = diccionario['color']
-        valores_x = diccionario['valores_x']
-        valores_y = diccionario['valores_y']
+        valores_x = diccionario['muestra']
+        valores_y = lista_y
         label = diccionario['nombre'] if len(diccionarios) > 1 else ""
         plt.hlines(0, min(valores_x), max(valores_x), colors='k')
         plt.plot(valores_x, valores_y, 'o', color=color, label=label)
@@ -36,40 +44,41 @@ def dibujar(diccionarios, titulo_ventana, unidad_medida):
     plt.show()
 
 
-def __establecer_valores(diccionarios):
-    """
-    Transforma un diccionario en otro.
+def __establecer_valores_y(muestras: list,
+                           num_diccs: int) -> list[list]:
+    """Establece lista de valores 'y' de la grafica.
 
-    Retorno
-    -------
-    diccionarios: Diccionarios que contiene
-    - nombre: Nombre de la muestra
-    - color: Color de la muestra
-    - valores_x: Valores en la coordenada
-    x de los puntos de la muestra
-    - valores_y: Valores en la coordenada
-    y de los puntos de la muestra
-
-    Parametros
+    Parameters
     ----------
-    diccionarios: Diccionarios que contiene
-    - nombre: Nombre de la muestra
-    - color: Color de los puntos
-    - muestra: Arreglo de datos
-    titulo_ventana: Titulo de la ventana
-    unidad_medida: Unidad de la medida
+    muestras
+        Lista de todos los valores de la muestras
+    num_diccs
+        Numero de diccionarios
+
+    Returns
+    -------
+    list[list]
+        Lista que contiene una lista por cada muestra
 
     """
-    vals_x = [val for dic in diccionarios for val in dic['muestra']]
+    vals_x = muestras
     vals_y = [vals_x[0:i+1].count(vals_x[i])-1 for i in range(len(vals_x))]
-    num_elem = len(vals_y)//len(diccionarios)
-    listas_y = [vals_y[i:i+num_elem] for i in range(0, len(vals_y), num_elem)]
-    for diccionario, lista_y in zip(diccionarios, listas_y):
-        diccionario['valores_x'] = diccionario.pop('muestra')
-        diccionario['valores_y'] = lista_y
-    return diccionarios
+    num_elem = len(vals_y)//num_diccs
+    return [vals_y[i:i+num_elem] for i in range(0, len(vals_y), num_elem)]
 
 
-def __establecer_limite_superior(diccionarios):
-    vals_x = [val for dic in diccionarios for val in dic['muestra']]
-    return max([vals_x.count(val) for val in vals_x])
+def __establecer_limite_superior(muestras: list) -> int:
+    """Calcula el limite superior de la grafica.
+
+    Parameters
+    ----------
+    muestras
+        Lista de todos los valores de la muestras
+
+    Returns
+    -------
+    int
+        Limite superior
+
+    """
+    return max([muestras.count(val) for val in muestras])
