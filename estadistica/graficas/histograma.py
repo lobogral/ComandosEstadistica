81ab4cabcec659dfe3f0a20be38e9a0faa278a19
Camilo Sabogal <lobogral@gmail.com>
2021-10-12 17:07:47 -0500

    Mejoro calidad del codigo de graficas con linters

--- estadistica/graficas/histograma.py
+++ estadistica/graficas/histograma.py
@@ -1,30 +1,50 @@
-"""
-Realiza una grafica de histograma
-"""
+"""Realiza una grafica de histograma."""
 
 from matplotlib import pyplot as plt
+from redondeo.redondeo import redondear
 
 
-def dibujar(diccionarios, titulo_ventana, unidad_medida):
+def dibujar(diccionario: dict,
+            titulo_ventana: str,
+            unidad_medida: str) -> None:
+    """Realiza lo descrito en el docstring del modulo.
 
-    """
-    Realiza lo descrito en el docstring del modulo
-
-    Parametros
+    Parameters
     ----------
-    diccionarios: Diccionarios que contiene
-    - nombre: Nombre de la muestra
-    - color: Color de los puntos
-    - muestra: Arreglo de datos
-    titulo_ventana: Titulo de la ventana
-    unidad_medida: Unidad de la medida
+    diccionario
+        Diccionario que contiene lo siguiente
+        {
+            "muestra": muestra,
+            "paso": paso,
+            "minimo": minimo,
+            "maximo": maximo,
+            "num_div": num_div
+        }
+    titulo_ventana
+        Titulo de la ventana
+    unidad_medida
+        Unidad de la medida
 
     """
+    muestra = diccionario['muestra']
+    paso = diccionario['paso']
+    minimo = diccionario['minimo']
+    maximo = diccionario['maximo']
+    num_div = diccionario['num_div']
+
+    div = (maximo-minimo)/num_div
+
+    valores_x = []
+    valores_y = []
+    for i in range(num_div):
+        min_cls = minimo + div*i
+        max_cls = minimo + div*(i+1) - paso
+        frec = len([dato for dato in muestra if min_cls <= dato <= max_cls])
+        valores_x += [(min_cls+max_cls)/2]
+        valores_y += [redondear(frec/len(muestra), 3)]
 
     plt.figure(titulo_ventana)
     plt.xlabel(unidad_medida)
     plt.ylabel("Probabilidad")
-    valores_x = diccionarios['valores_x']
-    valores_y = diccionarios['valores_y']
     plt.bar(valores_x, valores_y, width=1, edgecolor='black')
     plt.show()
--- estadistica/tabla_frecuencias.py
+++ estadistica/tabla_frecuencias.py
@@ -1,96 +1,45 @@
+"""Ofrece opciones para las tablas de frecuencias."""
 from redondeo.redondeo import redondear
 
 
-def __obtener_tabla(datos, paso, minimo, maximo, num_divisiones):
+def imprimir_tabla(diccionario: dict) -> None:
+    """Obtiene una tabla de frecuencias.
 
-    """
-    Obtiene los siguientes datos:
-    - Intervalo de clase
-    - Punto medio
-    - Frecuencia
-    - Frecuencia Relativa
-
-    Parametros
+    Parameters
     ----------
-    datos: Arreglo de datos
-    paso: Distancia minima entre un valor y otro
-    minimo: Minimo valor admitido
-    maximo: Maximo valor admitido
-    num_divisiones: Numero de divisiones
+    diccionario
+        Diccionario que contiene lo siguiente
+        {
+            "muestra": muestra,
+            "paso": paso,
+            "minimo": minimo,
+            "maximo": maximo,
+            "num_div": num_div
+        }
 
     """
+    minimo = diccionario['minimo']
+    maximo = diccionario['maximo']
 
-    tabla = []
-    div = (maximo-minimo)/num_divisiones
-    for i in range(num_divisiones):
-        min_cls = minimo + div*i
-        max_cls = minimo + div*(i+1) - paso
-        frec = len([dato for dato in datos if min_cls <= dato <= max_cls])
-        tabla += [{
-            'int_cls': f'{min_cls}-{max_cls}',
-            'pnt_med': str((min_cls+max_cls)/2),
-            'frec': frec,
-            'frec_rel': redondear(frec/len(datos), 3)
-        }]
-    return tabla
-
-
-def imprimir_tabla(datos, paso, minimo, maximo, num_divisiones):
-
-    """
-    Imprime los siguientes datos:
-    - Intervalo de clase
-    - Punto medio
-    - Frecuencia
-    - Frecuencia Relativa
-
-    Parametros
-    ----------
-    datos: Arreglo de datos
-    paso: Distancia minima entre un valor y otro
-    minimo: Minimo valor admitido
-    maximo: Maximo valor admitido
-    num_divisiones: Numero de divisiones
-
-    """
-
-    tabla = __obtener_tabla(datos, paso, minimo, maximo, num_divisiones)
     str_int_cls = "Int. Cls.     "
     str_pnt_med = "Pnt. Med.    "
     str_frec = "Frec.    "
     str_frec_rel = "Frec. Rel."
-
     print(f'{str_int_cls}{str_pnt_med}{str_frec}{str_frec_rel}')
-    for diccionario in tabla:
 
-        int_cls = diccionario['int_cls']
-        pnt_med = diccionario['pnt_med']
-        frec = str(diccionario['frec'])
-        frec_rel = str(diccionario['frec_rel'])
+    div = (maximo-minimo)/diccionario['num_div']
+    for i in range(diccionario['num_div']):
+        min_cls = minimo + div*i
+        max_cls = minimo + div*(i+1) - diccionario['paso']
 
+        int_cls = f'{min_cls}-{max_cls}'
+        pnt_med = (min_cls+max_cls)/2
+        frec = len([dato
+                    for dato in diccionario['muestra']
+                    if min_cls <= dato <= max_cls])
+
+        frec_rel = redondear(frec/len(diccionario['muestra']), 3)
         print(f'{int_cls:{len(str_int_cls)}}', end="")
-        print(f'{pnt_med:{len(str_pnt_med)}}', end="")
-        print(f'{frec:{len(str_frec)}}', end="")
+        print(f'{str(pnt_med):{len(str_pnt_med)}}', end="")
+        print(f'{str(frec):{len(str_frec)}}', end="")
         print(frec_rel)
-
-
-def establecer_datos_hist(datos, paso, minimo, maximo, num_divisiones):
-
-    """
-    Cambia el formato para que se pueda graficar en un histograma:
-
-    Parametros
-    ----------
-    datos: Arreglo de datos
-    paso: Distancia minima entre un valor y otro
-    minimo: Minimo valor admitido
-    maximo: Maximo valor admitido
-    num_divisiones: Numero de divisiones
-
-    """
-
-    tabla = __obtener_tabla(datos, paso, minimo, maximo, num_divisiones)
-    hist = {}
-    hist['valores_x'] = [diccionario['pnt_med'] for diccionario in tabla]
-    hist['valores_y'] = [diccionario['frec_rel'] for diccionario in tabla]
-    return hist
