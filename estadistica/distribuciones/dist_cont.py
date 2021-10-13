3ad154af8a011f3b12ab0acc57a1ec73adf6473f
Camilo Sabogal <lobogral@gmail.com>
2021-10-13 18:16:22 -0500

    Retiro metodo privado innecesario

--- estadistica/distribuciones/dist_cont.py
+++ estadistica/distribuciones/dist_cont.py
@@ -29,30 +29,6 @@ def establecer_fdp(fdp_nuevo: Expr) -> None:
     FDP = fdp_nuevo
 
 
-def __agregar_relacion(expresion: Expr,
-                       relacion: Rel) -> Piecewise:
-    """Crea una funcion a trozos con la relacion.
-
-    La intencion es crear una nueva expresion (Piecewise),
-    en la cual la expresion actual es valida en la relacion
-    planteada y fuera de esta la nueva expresion es 0.
-
-    Parameters
-    ----------
-    expresion
-        Expresion actual
-    relacion
-        Relacion a la que se aplica la expresion
-
-    Returns
-    -------
-    Piecewise
-        Nueva expresion (Funcion a trozos resultante)
-    """
-    funcion_trozos = Piecewise((expresion, relacion), (0, True))
-    return piecewise_fold(funcion_trozos)
-
-
 def prob(relacion: Rel) -> Expr:
     """Calcula la probabilidad sobre una relacion de la FDP.
 
@@ -66,8 +42,9 @@ def prob(relacion: Rel) -> Expr:
     Expr
         Expresion resultante sin Symbols
     """
-    nueva_func = __agregar_relacion(FDP, relacion)
-    return prob_total(nueva_func)
+    func_troz = Piecewise((FDP, relacion), (0, True))
+    func_troz_simpl = piecewise_fold(func_troz)
+    return prob_total(func_troz_simpl)
 
 
 def prob_total(fdp_pru: Expr) -> Expr:
