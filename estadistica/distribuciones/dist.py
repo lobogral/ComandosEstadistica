7664668d0777c60b1061246a2d54e6ae96a9f9d6
Camilo Sabogal <lobogral@gmail.com>
2021-10-14 11:30:29 -0500

    Retiro variable global en dist_cont agregando clases

---
+++ estadistica/distribuciones/dist.py
@@ -0,0 +1,66 @@
+"""Ofrece una clase para manejo de distribuciones."""
+from abc import ABC
+from abc import abstractmethod
+from sympy import Piecewise
+from sympy import piecewise_fold
+from sympy import Rel
+from sympy import Expr
+
+
+class Dist(ABC):
+    """Ofrece funciones heredables para distribuciones.
+
+    Attributes
+    ----------
+    func_dist
+        Funcion de distribucion
+
+    Note
+    ----
+    De ahora en adelante las palabras funcion de distribucion
+    seran simplificadas como FD
+    """
+
+    func_dist: Expr = None
+
+    def est_func_dist(self, func_dist: Expr) -> None:
+        """Establece la FD.
+
+        Parameters
+        ----------
+        func_dist
+            Expresion de FD nueva
+        """
+        self.func_dist = func_dist
+
+    def prob(self, relacion: Rel) -> Expr:
+        """Calcula la probabilidad sobre una relacion de la FD.
+
+        Parameters
+        ----------
+        relacion
+            Relacion a la que se aplica el FD
+
+        Returns
+        -------
+        Expr
+            Expresion resultante sin Symbols
+        """
+        func_troz = Piecewise((self.func_dist, relacion), (0, True))
+        func_troz_simpl = piecewise_fold(func_troz)
+        return self.prob_total(func_troz_simpl)
+
+    @abstractmethod
+    def prob_total(self, func_dist_pru: Expr) -> Expr:
+        """Calcula la probabilidad total de una FD.
+
+        Parameters
+        ----------
+        func_dist_pru
+            FD de prueba
+
+        Returns
+        -------
+        Expr
+            Expresion resultante sin Symbols
+        """
--- estadistica/distribuciones/dist_cont.py
+++ estadistica/distribuciones/dist_cont.py
@@ -1,77 +1,34 @@
-"""Comandos para una distribucion continua.
-
-La distibucion continua tambien es llamada
-funcion de densidad de probabilidad (fdp)
-"""
+"""Comandos para una distribucion continua."""
 from sympy import Piecewise
 from sympy import oo
 from sympy import simplify
 from sympy import integrate
 from sympy import Symbol
-from sympy import piecewise_fold
-from sympy import Rel
 from sympy import Expr
 from sympy.abc import t
+from estadistica.distribuciones.dist import Dist
 
 
-FDP: Expr = None
+class DistCont(Dist):
+    """Ofrece funciones para distribuciones continuas."""
 
+    def prob_total(self, func_dist_pru: Expr) -> Expr:
+        """Calcula la probabilidad total de una FD.
 
-def establecer_fdp(fdp_nuevo: Expr) -> None:
-    """Establece la FDP.
+        Para mas informacion revisar
+        docstring de la clase padre (Dist)
+        """
+        var_pru, = func_dist_pru.atoms(Symbol)
+        return integrate(func_dist_pru, (var_pru, -oo, oo))
 
-    Parameters
-    ----------
-    fdp_nuevo
-        Expresion de densidad nueva
-    """
-    global FDP
-    FDP = fdp_nuevo
+    def prob_acum(self) -> Piecewise:
+        """Calcula la probabilidad acumulada de la FD.
 
-
-def prob(relacion: Rel) -> Expr:
-    """Calcula la probabilidad sobre una relacion de la FDP.
-
-    Parameters
-    ----------
-    relacion
-        Relacion a la que se aplica el FDP
-
-    Returns
-    -------
-    Expr
-        Expresion resultante sin Symbols
-    """
-    func_troz = Piecewise((FDP, relacion), (0, True))
-    func_troz_simpl = piecewise_fold(func_troz)
-    return prob_total(func_troz_simpl)
-
-
-def prob_total(fdp_pru: Expr) -> Expr:
-    """Calcula la probabilidad total de una fdp.
-
-    Parameters
-    ----------
-    fdp_pru
-        Fdp de prueba
-
-    Returns
-    -------
-    Expr
-        Expresion resultante sin Symbols
-    """
-    var_pru, = fdp_pru.atoms(Symbol)
-    return integrate(fdp_pru, (var_pru, -oo, oo))
-
-
-def prob_acum() -> Piecewise:
-    """Calcula la probabilidad acumulada de la FDP.
-
-    Returns
-    -------
-    Piecewise
-        Funcion a trozos resultante
-    """
-    var, = FDP.atoms(Symbol)
-    integral = integrate(FDP.subs(var, t), (t, -oo, var))
-    return simplify(integral.rewrite(Piecewise))
+        Returns
+        -------
+        Piecewise
+            Funcion a trozos resultante
+        """
+        var, = self.func_dist.atoms(Symbol)
+        integral = integrate(self.func_dist.subs(var, t), (t, -oo, var))
+        return simplify(integral.rewrite(Piecewise))
