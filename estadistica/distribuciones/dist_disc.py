0d782db51a64002814fc581c81c73a1c6d432afe
Camilo Sabogal <lobogral@gmail.com>
2021-10-14 12:21:33 -0500

    Retiro variable global en dist_disc agregando clases

--- estadistica/distribuciones/dist_disc.py
+++ estadistica/distribuciones/dist_disc.py
@@ -1,148 +1,46 @@
-"""Comandos para una distribucion discreta (dp)."""
+"""Comandos para una distribucion discreta."""
 from sympy import Piecewise
 from sympy import Symbol
 from sympy import summation
-from sympy import piecewise_fold
-from sympy import Rel
 from sympy import Range
-from sympy import solveset
-from sympy import Eq
-from sympy import Integers
-from sympy import EmptySet
 from sympy import Expr
+from estadistica.distribuciones.dist import Dist
+from estadistica.distribuciones.transf import establecer_dominio
 
 
-DP: Expr = None
+class DistDisc(Dist):
+    """Ofrece funciones para distribuciones discretas."""
 
+    def prob_total(self, func_dist_pru: Expr) -> Expr:
+        """Calcula la probabilidad total de una FD.
 
-def establecer_dp(dp_nuevo: Expr) -> None:
-    """Establece la DP.
+        Para mas informacion revisar
+        docstring de la clase padre (Dist)
+        """
+        dom_pru = establecer_dominio(func_dist_pru)
+        var, = func_dist_pru.atoms(Symbol)
+        if isinstance(dom_pru[var], Range):
+            inicio, final, _ = dom_pru[var].args
+            return summation(func_dist_pru, (var, inicio, final - 1))
+        vals = dom_pru[var]
+        return sum([func_dist_pru.subs(var, val) for val in vals])
 
-    Parameters
-    ----------
-    dp_nuevo
-        Expresion de distribucion nueva
-    """
-    global DP
-    DP = dp_nuevo
+    def prob_acum(self) -> Piecewise:
+        """Calcula la probabilidad acumulada de la FD.
 
+        Returns
+        -------
+        Piecewise
+            Funcion a trozos resultante
+        """
+        var, = self.func_dist.atoms(Symbol)
+        dom = establecer_dominio(self.func_dist)
+        vals = sorted(list(dom[var]))
 
-def prob(relacion: Rel) -> Expr:
-    """Calcula la probabilidad sobre una relacion de la DP.
-
-    Parameters
-    ----------
-    relacion
-        Relacion a la que se aplica el DP
-
-    Returns
-    -------
-    Expr
-        Expresion resultante sin Symbols
-    """
-    func_troz = Piecewise((DP, relacion), (0, True))
-    func_troz_simpl = piecewise_fold(func_troz)
-    return prob_total(func_troz_simpl)
-
-
-def prob_total(dp_pru):
-    """Calcula la probabilidad total de una dp.
-
-    Parameters
-    ----------
-    dp_pru
-        Dp de prueba
-
-    Returns
-    -------
-    Expr
-        Expresion resultante sin Symbols
-    """
-    dom_pru = __establecer_dominio(dp_pru)
-    var, = dp_pru.atoms(Symbol)
-    if isinstance(dom_pru[var], Range):
-        inicio, final, _ = dom_pru[var].args
-        return summation(dp_pru, (var, inicio, final - 1))
-    vals = dom_pru[var]
-    return sum([dp_pru.subs(var, val) for val in vals])
-
-
-def prob_acum() -> Piecewise:
-    """Calcula la probabilidad acumulada de la DP.
-
-    Returns
-    -------
-    Piecewise
-        Funcion a trozos resultante
-    """
-    var, = DP.atoms(Symbol)
-    dom = __establecer_dominio(DP)
-    vals = sorted(list(dom[var]))
-
-    lista = []
-    for i, _ in enumerate(vals):
-        sub_suma = sum([DP.subs(var, val) for val in vals[:i]])
-        llave = vals[i]
-        lista += [(sub_suma, (var < llave))]
-    lista += [(1, True)]
-    return Piecewise(*lista)
-
-
-def __establecer_dominio(dist_prob: Expr) -> dict:
-    """Establece el dominio a partir de una dp.
-
-    Parameters
-    ----------
-    dist_prob
-        Distribucion de probabilidad
-
-    Returns
-    -------
-    dict
-        Dominio
-    """
-    var, = dist_prob.atoms(Symbol)
-    equations = dist_prob.atoms(Eq)
-    orders = dist_prob.atoms(Rel) - equations
-    dom = {var: EmptySet}
-    for order in orders:
-        val = solveset(order, var, Integers)
-        dom[var] = dom[var] & val if dom[var] else val
-    for equation in equations:
-        val = solveset(equation, var)
-        dom[var] = dom[var] | val
-    return dom
-
-
-def dp_a_dist() -> dict:
-    """Transforma la expresion DP a un diccionario.
-
-    Returns
-    -------
-    dict
-        Distribucion en forma de diccionario
-    """
-    dom = __establecer_dominio(DP)
-    var, = dom.keys()
-    vals: list = list(*dom.values())
-    return {val: DP.subs({var: val}) for val in vals}
-
-
-def dist_a_dp(dist: dict,
-              var: Symbol) -> Expr:
-    """Transforma un diccionario (dist) a una expresion (dp).
-
-    Parameters
-    ----------
-    dist
-        Diccionario de distribucion de probabilidad
-    var
-        Variable
-
-    Returns
-    -------
-    Expr
-        Distribucion en forma de expresion
-    """
-    lista_troz_dp = [(v, Eq(var, k)) for k, v in dist.items()]
-    return Piecewise(*lista_troz_dp)
+        lista = []
+        for i, _ in enumerate(vals):
+            sub_suma = sum([self.func_dist.subs(var, val) for val in vals[:i]])
+            llave = vals[i]
+            lista += [(sub_suma, (var < llave))]
+        lista += [(1, True)]
+        return Piecewise(*lista)
---
+++ estadistica/distribuciones/transf.py
@@ -0,0 +1,69 @@
+"""Ofrece funcionalidades de transformacion."""
+from sympy import Piecewise
+from sympy import Symbol
+from sympy import Rel
+from sympy import solveset
+from sympy import Eq
+from sympy import Integers
+from sympy import EmptySet
+from sympy import Expr
+
+
+def establecer_dominio(func_dist: Expr) -> dict:
+    """Establece el dominio a partir de una FD.
+
+    Parameters
+    ----------
+    func_dist
+        Distribucion de probabilidad
+
+    Returns
+    -------
+    dict
+        Dominio
+    """
+    var, = func_dist.atoms(Symbol)
+    equations = func_dist.atoms(Eq)
+    orders = func_dist.atoms(Rel) - equations
+    dom = {var: EmptySet}
+    for order in orders:
+        val = solveset(order, var, Integers)
+        dom[var] = dom[var] & val if dom[var] else val
+    for equation in equations:
+        val = solveset(equation, var)
+        dom[var] = dom[var] | val
+    return dom
+
+
+def dp_a_dist(func_dist: Expr) -> dict:
+    """Transforma la expresion FD a un diccionario.
+
+    Returns
+    -------
+    dict
+        Distribucion en forma de diccionario
+    """
+    dom = establecer_dominio(func_dist)
+    var, = dom.keys()
+    vals: list = list(*dom.values())
+    return {val: func_dist.subs({var: val}) for val in vals}
+
+
+def dist_a_dp(dist: dict,
+              var: Symbol) -> Expr:
+    """Transforma un diccionario (dist) a una expresion (FD).
+
+    Parameters
+    ----------
+    dist
+        Diccionario de distribucion de probabilidad
+    var
+        Variable
+
+    Returns
+    -------
+    Expr
+        Distribucion en forma de expresion
+    """
+    lista_troz_dp = [(v, Eq(var, k)) for k, v in dist.items()]
+    return Piecewise(*lista_troz_dp)
