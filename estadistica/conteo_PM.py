f0d6d70b82615a8d497f1d234e18bb8403dfcc62
Camilo Sabogal <lobogral@gmail.com>
2021-10-21 12:56:54 -0500

    Agrego documentacion a conteo_pm

--- estadistica/conteo_PM.py
+++ estadistica/conteo_PM.py
@@ -1,18 +1,89 @@
+"""Ofrece funcionalidades para conteo de puntos muestrales."""
 from math import prod
 
 
-def factorial(n):
-    return n * factorial(n-1) if n > 1 else 1
+def factorial(num: int) -> int:
+    """Calcula el factorial de un numero.
+
+    Parameters
+    ----------
+    num
+        Numero al que se le calcula el factorial
+
+    Returns
+    -------
+    int
+        Factorial
+
+    """
+    return num * factorial(num-1) if num > 1 else 1
 
 
-def nP(n, r):
-    return factorial(n) // factorial(n-r)
+def perms(num_elems: int, num_selec: int) -> int:
+    """Calcula permutaciones.
+
+    Calcula el numero de permutaciones de los elementos
+    de un conjunto tomados una cantidad de elementos
+    a la vez.
+
+    Parameters
+    ----------
+    num_elems
+        Numero de elementos de un conjunto
+    num_selec
+        Numero de elementos seleccionados a la vez
+
+    Return
+    ------
+    int
+        Numero de permutaciones
+
+    """
+    return factorial(num_elems) // factorial(num_elems-num_selec)
 
 
-def nT(n, list_args):
-    mult = prod([factorial(i) for i in list_args])
-    return factorial(n) // mult
+def perms_clase(num_elems: int, list_num_selec: list[int]) -> int:
+    """Calcula permutaciones de clase.
+
+    Calcula el numero de permutaciones de clase de los
+    elementos de un conjunto tomados en una lista
+    de grupos de elementos.
+
+    Parameters
+    ----------
+    num_elems
+        Numero de elementos de un conjunto
+    list_num_select
+        Lista de grupos de elementos
+
+    Return
+    ------
+    int
+        Numero de permutaciones de clase
+
+    """
+    mult = prod([factorial(i) for i in list_num_selec])
+    return factorial(num_elems) // mult
 
 
-def nC(n, r):
-    return factorial(n) // (factorial(r) * factorial(n-r))
+def combs(num_elems: int, num_selec: int) -> int:
+    """Calcula combinaciones.
+
+    Calcula el numero de combinaciones de los elementos
+    de un conjunto tomados una cantidad de elementos
+    a la vez.
+
+    Parameters
+    ----------
+    num_elems
+        Numero de elementos de un conjunto
+    num_selec
+        Numero de elementos seleccionados a la vez
+
+    Return
+    ------
+    int
+        Numero de combinaciones
+
+    """
+    return perms_clase(num_elems, [num_selec, num_elems-num_selec])
