a74c2fa6317d3c1f6afd21a1eada769adf986898
Camilo Sabogal <lobogral@gmail.com>
2021-10-30 16:03:11 -0500

    Cambio find_namespace_packages por find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 setup(
     name="estadistica",
     version="0.2",
-    packages=find_namespace_packages(),
+    packages=find_packages(),
     install_requires = [
         'redondeo @ https://github.com/lobogral/redondeo/releases/latest/download/redondeo.tar.gz',
         'matplotlib',
