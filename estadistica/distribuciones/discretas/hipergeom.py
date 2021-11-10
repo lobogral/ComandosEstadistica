feb7f280518e4b4a5cf4e552e86fe294b6c1a6c3
Camilo Sabogal <lobogral@gmail.com>
2021-11-09 19:32:52 -0500

    Agrego distribucion hipergeometrica

---
+++ estadistica/distribuciones/discretas/hipergeom.py
@@ -0,0 +1,38 @@
+"""Ofrece una clase para manejo de la distribución hipergeométrica."""
+from estadistica.distribuciones.dist_disc import DistDisc
+from estadistica.distribuciones.dist_conc import DistConc
+from sympy import Piecewise
+from sympy import Expr
+from sympy import binomial as nC
+
+
+class Hipergeom(DistConc):
+    """Ofrece funcionalidades para distribución hipergeométrica."""
+
+    def __init__(self,
+                 var: Expr,
+                 num_vals: int,
+                 muestra: int,
+                 num_exi: int) -> None:
+        """Inicializa la clase.
+
+        Establece valores concretos para que se pueda
+        crear la distribución.
+
+        Parameters
+        ----------
+        var
+            Variable
+        num_vals
+            Número de valores
+        muestra
+            Muestra del número de valores
+        num_exi
+            Número de exitos
+        """
+        num_fra = num_vals - num_exi
+        func_dist = Piecewise((
+            (nC(num_exi, var)*nC(num_fra, muestra-var))/nC(num_vals, muestra),
+            (var >= max(0, muestra-num_fra)) & (var <= min(muestra, num_exi))))
+        self.dist = DistDisc()
+        self.dist.est_func_dist(func_dist)
